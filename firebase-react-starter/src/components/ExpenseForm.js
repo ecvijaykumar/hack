f2c48f36ca5626e1ef95605362f0b781f729abb1
vekkaladevi <ecvijay@hotmail.com>
2017-01-21 23:55:58 -0800

    Add auto complete and date fields

--- firebase-react-starter/src/components/ExpenseForm.js
+++ firebase-react-starter/src/components/ExpenseForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { reduxForm, Field } from 'redux-form'
-import { Paper, TextField, RaisedButton } from 'material-ui'
+import { Paper, TextField, RaisedButton, DatePicker, AutoComplete} from 'material-ui'
 
 const renderTextField = ({input, label, meta: { touched, error}, ...custom}) => (
   <div>
@@ -20,6 +20,46 @@ const renderButton = ({input, label, meta: { touched, error}, ...custom} ) => (
     />
   </div>
 )
+
+const renderCategory = () => {
+  const categories = [
+    'Groceries',
+    'Movies',
+    'Medical',
+    'Auto'
+  ]
+
+  const handleUpdateInput = (value) => {
+    console.log(value)
+
+  }
+  return(
+    <AutoComplete
+      hintText="Category"
+      filter={AutoComplete.fuzzyFilter}
+      dataSource={categories}
+      onUpdateInput={handleUpdateInput}
+      maxSearchResults={5}
+    />
+  )
+}
+
+const renderDate = () => {
+  let maxDate = new Date()
+  let minDate = new Date()
+  minDate.setFullYear(minDate.getFullYear() -1)
+  console.log(minDate)
+
+  return (
+    <DatePicker onChange={(unused, date) =>{ console.log(date)}}
+      floatingLabelText="Spent On"
+      defaultDate={maxDate}
+      minDate={minDate}
+      maxDate={maxDate}
+      autoOk={true}
+    />
+  )
+}
 
 const ExpenseForm = props => {
   const { handleSubmit } = props
@@ -45,8 +85,10 @@ const ExpenseForm = props => {
           <Field name="amount" component={renderTextField} label="Amount"/>
           <Field name="item" component={renderTextField} label="Item"/>
           <Field name="location" component={renderTextField} label="Spent at"/>
-          <Field name="date" component={renderTextField} label="date"/>
-          <Field name="submit" component={renderButton} label="Add"
+          <Field name="date" component={renderDate} label="date"/>
+          <Field name="category" component={renderCategory} label="category"
+ />
+           <Field name="submit" component={renderButton} label="Add"
             style={{'width' : '100%'}}/>
 
         </form>
