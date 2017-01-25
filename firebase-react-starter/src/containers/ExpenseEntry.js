9e60c4c0400bf59ea1127be1c109c329b31373b4
vekkaladevi <ecvijay@hotmail.com>
2017-01-25 00:41:15 -0800

    Seperate expenses

---
+++ firebase-react-starter/src/containers/ExpenseEntry.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { Paper } from 'material-ui'
+import { MuiThemeProvider, getMuiTheme} from 'material-ui/styles'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { newExpense } from '../actions/ExpenseActionCreators'
+import ExpenseForm  from '../components/ExpenseForm'
+import ExpenseSummary from '../components/ExpenseSummary'
+
+
+const ExpenseEntry = props => (
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <Paper style={{margin: 30}}>
+        <ExpenseForm onSubmit={props.addExpense}
+            items={props.expenseFields.items}
+            at={props.expenseFields.at}/>
+        <ExpenseSummary total={props.total}/>
+      </Paper>
+    </MuiThemeProvider>
+)
+
+const mapStateToProps = (state) => (
+  {
+    total: state.total,
+    expenseFields: state.expenseFields
+  }
+)
+
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    addExpense: newExpense
+  }, dispatch)
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseEntry)
